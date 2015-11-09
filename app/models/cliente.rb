53afef05a0134244bca2037a9ee0926332250c92
jefferson abreu <nabucodonosor310@gmail.com>
2015-11-09 09:28:22 -0200

    criando 3 novas migracoes

---
+++ app/models/cliente.rb
@@ -0,0 +1,6 @@
+class Cliente < ActiveRecord::Base
+   validates_presence_of :nome, message: " - Deve ser preenchido"
+   validates_uniqueness_of :nome, message: " - Nome já cadastrado"
+   validates_numericality_of :idade, greater_than: 0, less_than: 100, message: " - deve ser um numero entre 0 e 100"
+
+end
--- app/models/restaurante.rb
+++ app/models/restaurante.rb
@@ -1,2 +1,16 @@
 class Restaurante < ActiveRecord::Base
+    validates_presence_of :nome, message: "deve ser preenchido"
+    validates_presence_of :endereco, message: "deve ser preenchido"
+    validates_presence_of :especialidade, message: "deve ser preenchido"
+
+    validates_uniqueness_of :nome, message: "nome já cadastrado"
+    validates_uniqueness_of :endereco, message: "endereço já cadastrado"    
+
+    validate :primeira_letra_maiuscula
+    
+    private
+    def primeira_letra_maiuscula
+        errors.add(:nome, "a letra deve iniciar com maiuscula") unless nome =~/[A-Z].*/   
+    end
+
 end
