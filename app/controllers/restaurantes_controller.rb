b1f859cc5b59301e3baac014c543e985a88c6a2a
jefferson abreu <nabucodonosor310@gmail.com>
2015-11-20 22:37:10 -0200

    adcionado cadastro de restaurante

--- app/controllers/restaurantes_controller.rb
+++ app/controllers/restaurantes_controller.rb
@@ -7,4 +7,26 @@ class RestaurantesController < ApplicationController
          @restaurante = Restaurante.find(params[:id])   
     end
 
+    def destroy
+        @restaurante = Restaurante.find(params[:id])
+        @restaurante.destroy
+
+        redirect_to(action: "index")    
+    end
+
+    def new
+        @restaurante = Restaurante.new
+    end
+    
+    def restaurante_params
+        params.require(:restaurante).permit(:nome, :endereco, :especialidade)    
+    end
+
+    def create
+        @restaurante = Restaurante.new(restaurante_params)   
+        
+        @restaurante.save
+        redirect_to(action: "show", id: @restaurante )
+    end
+
 end
