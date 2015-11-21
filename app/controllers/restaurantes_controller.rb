fa4ab6db54f8ea4f3f35299dc0e54b1b65cc1c84
jefferson abreu <nabucodonosor310@gmail.com>
2015-11-21 17:33:19 -0200

    adcionado retorno e edicao

--- app/controllers/restaurantes_controller.rb
+++ app/controllers/restaurantes_controller.rb
@@ -25,8 +25,21 @@ class RestaurantesController < ApplicationController
     def create
         @restaurante = Restaurante.new(restaurante_params)   
         
-        @restaurante.save
-        redirect_to(action: "show", id: @restaurante )
+       if @restaurante.save
+            redirect_to(action: "show", id: @restaurante )
+        else
+            redirect_to action: "new"        
+        end
     end
 
+    def edit
+       @restaurante = Restaurante.find(params[:id])
+    end
+
+    def update
+        @restaurante = Restaurante.find(params[:id])
+        @restaurante.update_attributes(restaurante_params)
+        
+        redirect_to action: "show", id: @restaurante    
+    end
 end
